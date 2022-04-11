39c26e30b5eb0575a13ffecc6bfa976f37c77e7c
Natalia <natinho@yandex.ru>
2022-04-12 01:37:11 +0300

    in process

--- src/main/kotlin/ru/netology/DirectMessageService.kt
+++ src/main/kotlin/ru/netology/DirectMessageService.kt
@@ -3,10 +3,11 @@ package ru.netology
 object DirectMessageService {
     private val directMessages = mutableListOf<DirectMessage>()
 
-    fun addDirectMessages(owner: People, user: People): Boolean {
+    fun addDirectMessages(owner: People, user: People, text: String): Boolean {
         for (chat in directMessages) {
             if (chat.owner == owner && chat.user == user && !chat.isDelete) {
                 println("Чат открыт")
+
                 return true
             } else if (chat.owner == owner && chat.user == user && chat.isDelete) {
                 chat.isDelete = false
@@ -21,15 +22,58 @@ object DirectMessageService {
         return true
     }
 
+    fun createMessageFrom(text: String, directMessage: DirectMessage, owner: People, user: People) {
+        if (directMessage.message.isNullOrEmpty()) {
+            val idMessage = 1U
+            val message = Message(
+                idMessage,
+                owner,
+                user,
+                text,
+                System.currentTimeMillis(),
+                isDelete = false,
+                ownerRead = true,
+                userRead = false
+            )
+            directMessage.message?.add(message)
+        }
+    }
+
+    fun createMessageTo(text: String, directMessage: DirectMessage, owner: People, user: People) {
+        val idMessage = (directMessage.message?.lastOrNull()?.idMessage ?: 0U) + 1U
+        val message = Message(
+            idMessage,
+            user,
+            owner,
+            text,
+            System.currentTimeMillis(),
+            isDelete = false,
+            ownerRead = false,
+            userRead = true
+        )
+        directMessage.message?.add(message)
+    }
+
     fun deleteDirectMessages(owner: People, user: People) {
+        // val deletedChat = directMessages.find{owner:People , user:People -> }
         for (chat in directMessages) {
             if (chat.owner == owner && chat.user == user && !chat.isDelete) {
                 chat.isDelete = true
+                for (message in chat.message!!) {
+                    message.isDelete = true
+                }
                 println("Чат удален")
             }
-
+            if (chat.owner == user && chat.user == owner && !chat.isDelete) {
+                chat.isDelete = true
+                for (message in chat.message!!) {
+                    message.isDelete = true
+                }
+                println("Чат собеседника удален")
+            }
         }
     }
+
 }
 
 //    fun addMessage(message: Message, directMessage: DirectMessage): Boolean {
