389e578b27ee9668dc9f967efe88c2f86cf83296
Natalia <natinho@yandex.ru>
2022-04-15 16:49:15 +0300

    continue add lambdas

--- src/main/kotlin/ru/netology/DirectMessageService.kt
+++ src/main/kotlin/ru/netology/DirectMessageService.kt
@@ -137,150 +137,30 @@ object DirectMessageService {
             }
             .filter { (it.message?.count { message -> !message.isDelete } ?: 0) == 0 }
             .forEach { it.isDelete = true }
-
-
         return true
     }
 
-    //  fun deleteMessage(owner: People, targetPeople: People, deletedMessage: Message): Boolean {
-//        val editedChat = findChatInDirectMessages(owner, targetPeople, false)
-//        if (editedChat != null) {
-//            for (message in editedChat.message!!) {
-//                if (message.idMessage == deletedMessage.idMessage && !message.isDelete) {
-//                    message.isDelete = true
-//                    if (editedChat.message.count() == 1) {
-//                        editedChat.isDelete = true
-//                    }
-//                    editDirectMessages(editedChat)
-//                    return true
-//                }
-//            }
-//        }
-//        return false
-//    }
 
-//    fun getMessages(
-//        idChat: UInt,
-//        idMessageStart: UInt,
-//        numberOfMessages: Int,
-//        owner: People
-//    ): MutableList<Message> {
-//        val resultList = mutableListOf<Message>()
-//        directMessages.stream()
-//            .filter{ directMessages->directMessages.idChat == idChat && !directMessages.isDelete}
-//            .peek{ directMessages->directMessages.isDelete=true}
-//            .peek( directMessages-> )
-//            .collect{Collectors.toList<>()};
+    fun getMessages(
+        idChat: UInt,
+        idMessageStart: UInt,
+        numberOfMessages: Int,
+        owner: People
+    ): MutableList<Message> {
 
-//        val resultChat = directMessages.find { it.idChat == idChat && !it.isDelete }
-//        var numberMessage = numberOfMessages
-//        if (resultChat != null) {
-//            for (message in resultChat.message!!) {
-//                if (message.idMessage >= idMessageStart && !message.isDelete && numberMessage > 0) {
-//                    if (message.targetPeople == owner) {
-//                        message.targetPeopleIsRead = true
-//                    }
-//                    resultList.add(message)
-//                    numberMessage -= 1
-//                }
-//            }
-//        }
-//        return resultList
-//    }
+        val resultList1 = mutableListOf<Message>()
+        val resultChat1 = directMessages.find { it.idChat == idChat && !it.isDelete }
+        resultChat1?.message?.listIterator()
+        resultChat1?.message?.filter { it.idMessage >= idMessageStart && !it.isDelete }
+        resultChat1?.message?.filterIndexedTo(resultList1) { index, _ -> index <= numberOfMessages }
+
+        resultList1
+            .filter { it.targetPeople == owner }
+            .forEach { it.targetPeopleIsRead = true }
+
+        return resultList1
+    }
 }
-
-//    val cats = listOf("Мурзик", "Барсик", "Рыжик")
-//    println(cats.get(2))
-//    Но у этих способов есть один недостаток - если вы укажете неправильное значение индекса, то получите исключение ArrayIndexOutOfBoundsException. Вы можете избежать проблемы, если будете использовать метод getOrElse() с указанием значения по умолчанию, если выйдете за пределы допустимых значений. В этом случае вам не придётся обрабатывать исключение.
-//
-//
-//    val cats = listOf("Мурзик", "Барсик", "Рыжик")
-//    println(cats.getOrElse(4) { "Неизвестный котик" })
-//// или как вариант, имя первого кота
-//    println(cats.getOrElse(4) { cats.first() })
-//#######################
-
-
-//        for (chat in directMessages) {
-//            if ((chat.user1 == owner && chat.user2 == targetPeople && !chat.isDelete) ||
-//                (chat.user2 == owner && chat.user1 == targetPeople && !chat.isDelete)
-//            ) {
-//                createMessage(text, chat, targetPeople)
-//                println("Чат открыт")
-//                return true
-//            } else if ((chat.user1 == owner && chat.user2 == targetPeople && chat.isDelete) ||
-//                (chat.user2 == owner && chat.user1 == targetPeople && chat.isDelete)
-//            ) {
-//                chat.isDelete = false
-//                createMessage(text, chat, targetPeople)
-//                println("Чат открыт")
-//                return true
-//            }
-//        }
-//        val newId = (directMessages.lastOrNull()?.idChat ?: 0U) + 1U
-//        val directMessageAdding = DirectMessage(newId, owner, targetPeople, null, false)
-//        directMessages.add(directMessageAdding)
-//        createMessage(text, directMessageAdding, targetPeople)
-//        println("Чат создан")
-//        return true
-
-
-//for (chat in directMessages) {
-//            if ((chat.user1 == owner && chat.user2 == targetPeople && !chat.isDelete) ||
-//                (chat.user2 == owner && chat.user1 == targetPeople && !chat.isDelete)
-//            ) {
-//                chat.isDelete = true
-//                for (message in chat.message!!) {
-//                    message.isDelete = true
-//                }
-//                println("Чат удален")
-//            }
-//        }
-
-//        val resultList: MutableList<DirectMessage> = mutableListOf()
-//        for (chat in directMessages) {
-//            if ((chat.user1 == owner && !chat.isDelete) || (chat.user2 == owner && !chat.isDelete)) {
-//                resultList.add(chat)
-//            }
-//        }
-//        if (resultList.isEmpty()) {
-//            println("нет сообщений")
-//        }
-
-//    fun getUnreadChatsCount(owner: People): UInt {
-//        var countChat: UInt = 0U
-//        for (chat in directMessages) {
-//            if ((chat.user1 == owner && !chat.isDelete) || (chat.user2 == owner && !chat.isDelete)) {
-//                var flag = false
-//                for (message in chat.message!!) {
-//                    if (message.targetPeople == owner && !message.targetPeopleIsRead) {
-//                        flag = true
-//                    }
-//                }
-//                if (flag) {
-//                    countChat += 1U
-//                }
-//            }
-//        }
-//        return countChat
-//    }
-
-
-//        directMessages.stream()
-//            .filter{ directMessages->directMessages.idChat == idChat && !directMessages.isDelete}
-//            .peek{ directMessages->directMessages.isDelete=true}
-//            .peek( directMessages-> )
-//            .collect{Collectors.toList<>()};
-
-
-// private fun editDirectMessages(directMessage: DirectMessage) {
-//        for ((index) in directMessages.withIndex()) {
-//            if (directMessages[index].idChat == directMessage.idChat) {
-//                directMessages[index] = directMessage.copy(
-//                )
-//            }
-//        }
-//    }
 
 
 
